a742bfbb3a1435e2c54b0a8e3155bdab38331173
ivanDjatche <ivandjatche90@gmail.com>
2023-06-30 11:21:16 +0200

    update Readme file and view/app and config/app and route/web

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,15 +4,20 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Hello-City</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">     
     </head>
 
     <body>
+        <h1> Hello from Ouest Region Of Cameroun!!</h1>
 
-    <h2>BONJOUR ET BIEN VENUE DANS MON PROJET HELLO CITY NOUS SOMMES BELLE ET BIEN EN {{ date('Y') }}</h2>
-</body>
+        <p> It's currently {{ date('h:i:s A')}}.</p>
+
+        <footer>
+            <p>&copy; Copyright {{ date('Y')}} &middot; <a href="#">About-Us</a></p>
+        </footer>
+    </body>
  
 </html>
