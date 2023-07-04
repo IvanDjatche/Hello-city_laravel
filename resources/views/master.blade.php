916024e330ddb406df09b0f2b03951adfff7e10e
ivanDjatche <ivandjatche90@gmail.com>
2023-07-04 10:41:49 +0200

    remove dupplication an using master layout

---
+++ resources/views/master.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Hello-City</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">     
+    </head>
+
+    <body>
+        <!-- yield cree un bloc ou une section content qui sera utilisé au niveau des pages pour cree le contenu en fonction de la page -->
+        @yield('content')
+
+        <footer>
+        <!-- use the date function to display years -->
+            <p>&copy; Copyright {{ date('Y')}} &middot;
+                   <a href="{{ __('about-us') }}">About-Us</a>
+            </p>
+        </footer>  
+    </body>
+ 
+</html> 
--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,26 +1,13 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- extends permet d'heriter du layout de base ayant pour nom master  -->
+@extends('master')
 
-        <title>About-Us | Hello-City</title>
-
-       
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">     
-    </head>
-
-    <body>
+<!-- section permet de cree du contenu en fonction des diff pages grace a yield du layout de base  -->
+@section('content')
 
         <p>Built with &hearts; By Ivan Djatche Jordan Dev Web Laravel Fulstack</p>
          <!-- use the date function for display the currently time -->
         <p><a href="{{ __('/') }}">Revenir à la page d'accueil</a></p>
 
-        <footer>
-        <!-- use the date function to display years -->
-        <!-- move the about link on about page : first method -->
-            <p>&copy; Copyright {{ date('Y')}}</p>
-        </footer>  
-    </body>
- 
-</html> 
+@endsection
+
+
